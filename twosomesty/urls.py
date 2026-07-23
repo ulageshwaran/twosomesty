aca804d79bfce05eb753ce7f02bc257eb84e5dff
ulageshwaran <ulageshofficial@gmail.com>
2026-07-23 18:09:35 +0530

    login update

--- twosomesty/urls.py
+++ twosomesty/urls.py
@@ -22,16 +22,13 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('twosomeadmin/', admin.site.urls),
     path("silk/", include("silk.urls", namespace="silk")),
+    path('', include('store.urls')),
     path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns += [
-    path('', include('store.urls')),
-]
 
 
 handler404 = 'store.views.custom_404_view'
